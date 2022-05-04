729f39c14c0cd93b145f8ac12c79a9533521870f
Maciej Balawejder <maciekstar6@gmail.com>
2022-05-04 18:56:30 +0200

    add training functions

---
+++ Train.py
@@ -0,0 +1,40 @@
+from tqdm.notebook import tqdm_notebook
+from CIFAR10 import LoadDataset
+from ResNet import ResNet
+from TrainingFunctions import Network
+from Augmentations import GetAugment
+from Plots import plot
+
+# Parameters
+epochs = 40
+device = "cuda" if torch.cuda.is_available() else "cpu"
+learning_rate = 1e-3
+batch = 256
+n = 3
+
+""" Configurations is a list of plain(normalization), baseline, autoaugment augementations. """
+configurations = GetAugment()
+names = ["Plain", "Baseline", "AutoAugment"]
+normalization = configurations[0]
+
+
+""" Accuracies and Losses for each model. """
+accuracy = []
+loss = []
+
+""" Traning model for each augmentation -> Plain, Classic, RandAug. """
+for i in range(len(configurations)):
+    model = ResNet(n).to(device)
+    train_loader, val_loader, test_loader = LoadDataset(batch, normalization, configurations[i])
+    network = Network(model, learning_rate, device)
+    for epoch in tqdm_notebook(range(epochs), desc='Epoch'):
+        network.train_step(train_loader)
+        network.validation_step(val_loader)
+        print("{} => ".format(names[i])  + "Train acc : {}".format(network.train_acc[-1]), "Val acc : {}".format(network.val_acc[-1]))
+    network.test_step(test_loader)
+    network.save_all(names[i])
+    accuracy.append([network.train_acc, network.val_acc])
+    loss.append([network.train_loss, network.val_loss])
+
+plot(accuracy, "Accuracy")
+plot(loss, "Loss")
---
+++ TrainingFunctions.py
@@ -0,0 +1,113 @@
+class Network:
+    """
+    Network class that takes pytorch model, learning rate and device as input.
+
+    Parameters
+    ----------
+    loss -> loss function
+    opt -> optimizer
+    lr_scheduler -> learning rate scheduler which reduces lr when validation plateu
+
+    Functions
+    ---------
+
+    - train_step -> takes train_loader and train model for one epoch, and update train accuracy and loss
+    - validation_step -> takes val_loader and update validation accuracy and loss
+    - test_step -> takes test_loader and return averaged accuracy
+    - save_all -> saves traning, validation accuracy and loss to an '.npy' file
+
+    """
+
+    def __init__(
+        self,
+        model : torch.nn.Module,
+        learning_rate : float,
+        device : str,
+        ):
+
+        self.model = model
+        self.lr = learning_rate
+        self.loss = nn.CrossEntropyLoss()
+        self.opt = torch.optim.Adam(self.model.parameters(), lr = self.lr)
+        self.train_loss = []
+        self.val_loss = []
+        self.train_acc = []
+        self.val_acc = []
+        self.device = device
+
+        self.lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+                self.opt,
+                mode='min',
+                patience=3,
+                factor=0.5,
+                min_lr=1e-6,
+                verbose=True
+        )
+
+    def train_step(self, dataset):
+        self.model.train()
+        batch_loss = []
+        batch_acc = []
+        for batch in dataset:
+            inputs = batch[0].to(self.device)
+            targets = batch[1].to(self.device)
+            self.opt.zero_grad()
+
+            outputs = self.model(inputs)
+
+            loss = self.loss(outputs, targets)
+            loss.backward()
+            self.opt.step()
+            batch_loss.append(loss.item())
+            batch_acc.append(self.batch_accuracy(outputs,targets))
+
+        self.train_loss.append(np.mean(batch_loss))
+        self.train_acc.append(np.mean(batch_acc))
+
+
+    def validation_step(self, dataset):
+        self.model.eval()
+        batch_loss = []
+        batch_acc = []
+        with torch.no_grad():
+            for batch in dataset:
+                inputs = batch[0].to(self.device)
+                targets = batch[1].to(self.device)
+
+                outputs = self.model(inputs)
+
+                loss = self.loss(outputs, targets)
+                batch_loss.append(loss.item())
+                batch_acc.append(self.batch_accuracy(outputs,targets))
+
+        self.val_loss.append(np.mean(batch_loss))
+        self.val_acc.append(np.mean(batch_acc))
+        self.lr_scheduler.step(self.val_loss[-1])
+
+
+    def test_step(self, dataset):
+        self.model.eval()
+        batch_acc = []
+        with torch.no_grad():
+            for batch in dataset:
+                inputs = batch[0].to(self.device)
+                targets = batch[1].to(self.device)
+
+                outputs = self.model(inputs)
+                batch_acc.append(self.batch_accuracy(outputs,targets))
+
+        print("Accuracy : ", np.mean(batch_acc), "%")
+
+
+    def batch_accuracy(self, output, target):
+        # output shape: [batch, target]
+        output = nn.functional.softmax(output, dim=1)
+        output = output.argmax(1)
+        acc = torch.sum(output==target) / output.shape[0]
+        return acc.cpu()*100
+
+    def save_all(self, name):
+        np.save("val_acc{}.npy".format(name), self.val_acc)
+        np.save("train_acc{}.npy".format(name), self.train_acc)
+        np.save("val_loss{}.npy".format(name), self.val_loss)
+        np.save("train_loss{}.npy".format(name), self.train_loss)
